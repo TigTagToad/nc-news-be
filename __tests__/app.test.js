fe5fbf405b7a9849c4b63fb936f9811bce0a7c70
TigTagToad <taiga.arousse@gmail.com>
2024-11-25 14:04:22 +0000

    added article id endpoint

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -25,3 +25,36 @@ describe("GET /api", () => {
       });
   });
 });
+
+describe("GET /api/articles/:article_id", ()=>{
+  test("200: Responds with an object with relevent id", ()=>{
+    return request(app)
+    .get("/api/articles/1")
+    .expect(200)
+    .then((response)=>{
+      expect(response.body).toMatchObject({
+        article_id: 1,
+        title: "Living in the shadow of a great man",
+        topic: "mitch",
+        author: "butter_bridge",
+        body: "I find this existence challenging",
+        created_at: "2020-07-09T20:11:00.000Z",
+        votes: 100,
+        article_img_url:
+          "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
+      }
+      )
+    })
+
+  });
+  test("400: Responds with bad request when not valid id is used", ()=>{
+    return request(app)
+    .get("/api/articles/44")
+    .expect(400)
+    .then(({body})=>{
+      const {msg} = body;
+      expect(msg).toBe("bad request")
+    })
+  })
+
+});
--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const express = require("express");
 const app = express();
 const {getEndpoints} = require("./controllers/endpointsController")
+const {getArticle} = require("./controllers/articleController")
+
+const {postgresErrorHandler, customErrorHandler, serverErrorHandler} = require("./error-handling/errors")
 
 app.use(express.json());
 
-app.get("/api", getEndpoints)
+app.get("/api", getEndpoints);
+app.get("/api/articles/:article_id", getArticle);
+
+app.use(postgresErrorHandler);
+
+app.use(customErrorHandler);
+
+app.use(serverErrorHandler);
 
 module.exports = app
